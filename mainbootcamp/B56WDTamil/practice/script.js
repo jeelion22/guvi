daad09cb893563b75f7aa99e070fd001dec1ce5f
jeeva m <jeelion22@gmail.com>
2024-01-29 00:53:01 +0530

    invoke send() method, parse response as object, retrive capital and area

--- mainbootcamp/B56WDTamil/practice/script.js
+++ mainbootcamp/B56WDTamil/practice/script.js
@@ -1,4 +1,29 @@
 
 
-var request = new XMLHttpRequest() ;
+var request = new XMLHttpRequest();
+// initialize request
 request.open("GET", "https://restcountries.com/v3.1/all")
+
+// send request
+request.send()
+
+// once the data successfully received from the server
+// server code is 200
+// data from the server will a string that is json string
+// convert to string to object
+request.onload = function () {
+    var res = JSON.parse(request.response)
+    console.log(res)
+
+     for (i=0; i < 250; i++) {
+
+        if ("capital" in res[i]) {
+
+        console.log(i, res[i].capital[0], res[i].area)
+        }
+     }
+    // print all 250 country common name in console
+}
+
+//  capital name and area
+
