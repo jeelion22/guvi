86208e789c20b968fd581f24987e46ed0259e0ec
jeeva m <jeelion22@gmail.com>
2024-01-29 12:14:16 +0530

    first commit - day04 practice

---
+++ mainbootcamp/B56WDTamil/practice/printFirstNElements.js
@@ -0,0 +1,26 @@
+
+function printFirstNElements(arr, n = 0) {
+    if (n === 0) {
+        return arr[0]
+    }
+    
+    else if (n < 0) {
+        return []
+    }
+    else {
+        if (n == arr.length) {
+            return arr
+        }
+        
+        else {
+            return arr.slice(0, n)
+        }
+    }
+}
+
+
+
+console.log(printFirstNElements([1, 2, 3]))
+console.log(printFirstNElements([1, 2, 3], -4))
+console.log(printFirstNElements([1, 2, 3], 2))
+console.log(printFirstNElements([1, 2, 3], 8))
